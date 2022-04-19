b67ad52272812e68d34296bc056929192d5111f5
Craig Weston <craig.weston@landregistry.gov.uk>
2022-04-19 09:37:07 +0100

    adding mandatory employee fields, numeric fields and limits

--- app/validator/EmployeeValidator.js
+++ app/validator/EmployeeValidator.js
@@ -3,12 +3,16 @@ module.exports.validateEmployee = function (employee) {
         return "Salary must be a number"
     }
 
-    if (Number(employee.salary) < 20000) {
-        return "Salary must be at least £20,000"
+    if ((Number(employee.salary) < 20000 ) || (Number(employee.salary) > 200000)) {
+        return "Salary must be at least £20,000 and below £200,000"
     }
 
     if (employee.bankNo.length != 8) {
         return "Invalid bank number"
+    }
+
+    if ((employee.phoneNo.length < 11) || (employee.phoneNo.length) > 13) {
+        return "Please enter a valid phone number"
     }
 
     return null
